e706c30e03701df9436570466e9acf5e77a40efa
Mickie <mickystn10@gmail.com>
2023-04-22 22:36:54 +0700

    Update port.js

--- src/page/port.js
+++ src/page/port.js
@@ -11,66 +11,10 @@ import { message } from "antd";
 
 
 function port(){
-    const [userid,setUserid]=useState(34);
-
-    const [allport,setAllport]=useState([]);
-
-    const [addFrom,setAddfrom]=useState("");
-
-    useEffect(()=>{
-        Axios.get(`https://api-ea.vercel.app/allport`).then((res)=>{
-            setAllport(res.data);
-        })
-    },[])
-
-    const u_port = allport.filter((obj)=>{
-        return obj.user_id==userid;
-    }) 
-    //delete method
-    async function deletePort(port){
-        await Axios.delete(`https://api-ea.vercel.app/deletetran/${port}`).then((res) => {
-            console.log(res);
-        });
-        await Axios.delete(`https://api-ea.vercel.app/deleteOneport/${port}`).then((res) => {
-            console.log(res);
-        });
-    }
-
-    async function addPort(){
-        await Axios.post("https://api-ea.vercel.app/addport", {
-            Userid: userid,
-            portnumber: addFrom
-        }).then((response) => {
-            if (response.data.msg === "add port number success") {
-                message.success("Add port number success");
-            } else {
-                message.error("This port number already exists.");
-            }
-        });
-    }
-    function onChangePort(evt){
-        setAddfrom(evt.target.value);
-    }
+    
     return (
         <div>
-            <Navbar></Navbar>
-            <Button className ="btn" type="primary" size={"small"} onClick={addPort} >add port</Button>
-            <input onChange={onChangePort} placeholder="input port number"></input>
-            {
-                u_port.map((val)=>{
-                    return (
-                        <tr>
-                          <td>{val.user_id}</td>
-                          <td>{val.user_name}</td>
-                          <td>{val.email}</td>
-                          <td>{val.port_number}</td>
-                          <Link to="/Dashbord/port/transaction" state={{id:val.port_number}}>Show port</Link>
-                          <Button className ="btn" type="primary" size={"small"} onClick={()=>{deletePort(val.port_number)}}>delete</Button>
-                          
-                        </tr>
-                    )
-                })
-            }
+            <h>test</h>
         </div>
     )
 }
