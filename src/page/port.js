5a781eeed6bbb1d40e36876ca96fb0e84510626c
Mickie <mickystn10@gmail.com>
2023-04-22 22:43:18 +0700

    Update port.js

--- src/page/port.js
+++ src/page/port.js
@@ -1,18 +1,19 @@
 
-import "../css/home.css";
-import "../css/dashbord.css";
-import { Button } from "antd";
-import { useState,useEffect } from "react";
-import Navbar from "../component/navbar";
-import Axios from "axios";
-import { Link,useNavigate,createSearchParams } from "react-router-dom";
 
-import { message } from "antd";
+
+import { useState,useEffect } from "react";
+import Axios from "axios";
 
 
 function port(){
+    const [allport,setAllport]=useState([]);
 
-    const [addFrom,setAddfrom]=useState();
+
+    useEffect(()=>{
+        Axios.get(`https://api-ea.vercel.app/allport`).then((res)=>{
+            setAllport(res.data);
+        })
+    },[])
 
     return (
         <div>
