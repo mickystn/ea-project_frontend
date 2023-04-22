0312721db817201943f2efd82cf7da1fb9817b37
Mickie <mickystn10@gmail.com>
2023-04-22 22:39:47 +0700

    Update port.js

--- src/page/port.js
+++ src/page/port.js
@@ -17,12 +17,6 @@ function port(){
 
     const [addFrom,setAddfrom]=useState("");
 
-    useEffect(()=>{
-        Axios.get(`https://api-ea.vercel.app/allport`).then((res)=>{
-            setAllport(res.data);
-        })
-    },[])
-
     return (
         <div>
             <h>test</h>
